6badb4a852779bcf66059ef0523b2e19f720de13
Kyle Inzunza <inzukyle@gmail.com>
2020-08-15 16:36:27 -0400

    Copy paste access point works

--- wasp_demo_1/wasp_demo_1.ino
+++ wasp_demo_1/wasp_demo_1.ino
@@ -2,9 +2,8 @@
 #include <WiFi.h>
 
 // Replace with your network credentials
-//TODO: change to host own network
-const char* ssid = "archer-wifi";
-const char* password = "inzunzafast";
+const char* ssid     = "UWO CubeSat";
+const char* password = "123456789";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -20,13 +19,6 @@ String output27State = "off";
 const int output26 = 26;
 const int output27 = 27;
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -37,18 +29,14 @@ void setup() {
   digitalWrite(output27, LOW);
 
   // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
   server.begin();
 }
 
@@ -56,12 +44,9 @@ void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
+    while (client.connected()) {            // loop while the client's connected
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
